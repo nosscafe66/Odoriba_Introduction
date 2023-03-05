e7afa9194ff78cb1bf148b469db034059f6b6a53
nosscafe66 <nosscafe66@gmail.com>
2023-03-06 03:28:58 +0900

    フォームの送信処理を追加

--- pages/api/send.jsx
+++ pages/api/send.jsx
@@ -1,27 +1,27 @@
-export default async function handler(req, res) {  
-    if(req.method === 'POST') {
-      const sgMail = require('@sendgrid/mail');
-      sgMail.setApiKey("SG.xxx"); //SendGridのAPIキー
+// export default async function handler(req, res) {  
+//     if(req.method === 'POST') {
+//       const sgMail = require('@sendgrid/mail');
+//       sgMail.setApiKey("SG.xxx"); //SendGridのAPIキー
        
-      const msg = {
-        to: req.body.email,
-        from: 'pivot.tech.n.k@gmail.com',
-        subject: 'お問合せありがとうございました。',
-        text: 'お問合せを受け付けました。回答をお待ちください。' + req.body.message,
-        html: 'お問合せを受け付けました。回答をお待ちください。' + req.body.message,
-      };
+//       const msg = {
+//         to: req.body.email,
+//         from: 'pivot.tech.n.k@gmail.com',
+//         subject: 'お問合せありがとうございました。',
+//         text: 'お問合せを受け付けました。回答をお待ちください。' + req.body.message,
+//         html: 'お問合せを受け付けました。回答をお待ちください。' + req.body.message,
+//       };
    
-      (async () => {
-        try {
-          await sgMail.send(msg);
-        } catch (error) {
-          console.error(error);
-          if (error.response) {
-            console.error(error.response.body)
-          }
-        }
-      })();
-    }
+//       (async () => {
+//         try {
+//           await sgMail.send(msg);
+//         } catch (error) {
+//           console.error(error);
+//           if (error.response) {
+//             console.error(error.response.body)
+//           }
+//         }
+//       })();
+//     }
    
-    res.status(200)
-  }
+//     res.status(200)
+//   }
