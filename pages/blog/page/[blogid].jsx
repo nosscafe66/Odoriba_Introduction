0d875fd85e897312850f9b3670cbadae5d567e02
nosscafe66 <nosscafe66@gmail.com>
2023-03-13 10:59:37 +0900

    ページング処理追加

--- pages/blog/page/[blogid].jsx
+++ pages/blog/page/[blogid].jsx
@@ -11,7 +11,7 @@ export default function BlogPageId({ blog, totalCount }) {
             <ul>
                 {blog.map(blog => (
                     <li key={blog.id}>
-                        <Link href={`/blog/page/${blog.id}`}>{blog.title}</Link>
+                        <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
                     </li>
                 ))}
             </ul>
@@ -26,16 +26,16 @@ export const getStaticPaths = async () => {
     const range = (start, end) => [...Array(end - start + 1)].map((_, i) => start + i);
 
     // const paths = range(1, Math.ceil(repos.totalCount / PER_PAGE)).map((repo) => `/blog/page/${repo}`);
-    const paths = repos.contents.map((content) => `/blog/page/${content.id}`);
+    const paths = repos.contents.map((content) => `/blog/${content.id}`);
 
     return { paths, fallback: false };
 };
 
 // データを取得
 export const getStaticProps = async (context) => {
-    const id = context.params.blogid;
+    const blogid = context.params.blogid;
 
-    const data = await client.get({ endpoint: "blog", queries: { offset: (id - 1) * 5, limit: 5 } });
+    const data = await client.get({ endpoint: "blog", queries: { offset: (blogid - 1) * 5, limit: 5 } });
 
     return {
         props: {
