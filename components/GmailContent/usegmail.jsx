324a9644b327f2d897ef65de72a169a998271be9
nosscafe66 <nosscafe66@gmail.com>
2024-01-13 22:21:02 +0900

    IOSアプリ紹介LP作成

--- components/GmailContent/usegmail.jsx
+++
@@ -1,8 +0,0 @@
-async function getEmailList() {
-    const accessToken = await getAccessToken();
-    const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
-  
-    const { data: { messages } } = await gmail.users.messages.list({ userId: 'me', auth: oAuth2Client, maxResults: 10 });
-  
-    return messages;
-  }
